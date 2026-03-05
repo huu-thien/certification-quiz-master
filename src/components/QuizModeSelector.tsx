ef0ede8f7c19fc5ebde77a6cb5336957e7560eaa
Tên huu-thien <nhthien.dut@gmail.com>
2026-03-05 23:47:45 +0700

    [feat] npm run build

--- src/components/QuizModeSelector.tsx
+++ src/components/QuizModeSelector.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { BookOpen, Trophy, Lightbulb } from "lucide-react";
 import type { CourseConfig } from "../types";
 import {
